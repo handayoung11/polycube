dbf80a681917e92f74056b75b3d753bec33d6add
JinYeongKim <handayoung11@gmail.com>
2024-09-23 18:23:25 +0900

    /users/id api(user 조회 api) 작성

--- src/main/java/kr/co/polycube/backendtest/user/UserController.java
+++ src/main/java/kr/co/polycube/backendtest/user/UserController.java
@@ -1,6 +1,9 @@
 package kr.co.polycube.backendtest.user;
 
 import jakarta.validation.Valid;
+import kr.co.polycube.backendtest.user.db.UserRepo;
+import kr.co.polycube.backendtest.user.db.Users;
+import kr.co.polycube.backendtest.user.dto.GetUserResDTO;
 import kr.co.polycube.backendtest.user.dto.SignUpReqDTO;
 import kr.co.polycube.backendtest.user.dto.SignUpResDTO;
 import lombok.AllArgsConstructor;
@@ -13,10 +16,18 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     UserSvc userSvc;
+    UserRepo userRepo;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SignUpResDTO createUser(@ModelAttribute @Valid SignUpReqDTO user) {
         return userSvc.signUp(user);
+    }
+
+    @GetMapping("{id}")
+    public GetUserResDTO getUser(@PathVariable long id) {
+        Users user = userRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("User with id " + id + " not found"));
+        return new GetUserResDTO(user.getId(), user.getName());
     }
 }
